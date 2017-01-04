1d80d60843b5c8372ce330e4ef9f2789e6f16484
Ryan Smith <rsmith@molecularmatch.com>
2017-01-04 11:46:11 -0800

    removed potentially timeout issue while starting ES container on wercker

--- config/run.sh
+++ config/run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-#configure elasticsearch.yml with HOST_IP variable
-export LOCAL_IP=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
-sed -i "s/\HOST_IP/$LOCAL_IP/" /usr/share/elasticsearch/config/elasticsearch.yml
-sed -i "s/\CLUSTER_NAME/$CLUSTER_NAME/" /usr/share/elasticsearch/config/elasticsearch.yml
-
 /usr/share/elasticsearch/bin/elasticsearch
